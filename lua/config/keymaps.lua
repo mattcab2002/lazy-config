bedbd8173bdc6ca553a45a10522d8b6a8404d030
Matthew Cabral <matthew.larbac@gmail.com>
2024-02-28 10:26:29 -0500

    removing mini pairs and adding terminal keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>", { desc = "Terminal (cwd)" })
+vim.keymap.set("n", "<leader>``", "<cmd>ToggleTerm<cr>", { desc = "Terminal (cwd)" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,4 @@ vim.opt.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.opt.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.relativenumber = false
