950a378a7f14a6067a4430c2a7894e562b3b0225
Matthew Cabral <matthew.larbac@gmail.com>
2024-03-15 13:05:12 -0400

    new keymaps and centerpad

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>`", "<cmd>ToggleTerm<cr>", { desc = "Terminal (cwd)" })
+vim.keymap.set("n", "<leader>z", "<cmd>Centerpad<cr>", { silent = true, noremap = true })
